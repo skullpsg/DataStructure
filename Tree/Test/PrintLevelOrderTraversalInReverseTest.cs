029a66324b1fda9417647283b804bf0e6f78e78e
vishnu <vishnusankar.sakthivel@gmail.com>
2021-03-26 13:31:51 +0530

    Added level order traversal

---
+++ Tree/Test/PrintLevelOrderTraversalInReverseTest.cs
@@ -0,0 +1,32 @@
+﻿using Datastructure.Common;
+using Datastructure.Tree.Problems;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Datastructure.Tree.Test
+{
+    [TestFixture]
+    public class PrintLevelOrderTraversalInReverseTest
+    {
+        [Test]
+        public void ShouldPrintTreeInLevelOrder()
+        {
+            var expectedOutputs = new List<int>() { 6, 5, 4, 3, 2, 1 };
+            IConsoleWriter writerMock = MockWriter.GetInstance(expectedOutputs);
+            var rootNode = TreeTestData.GetSimpleTreeRootNode();
+            var levelOrderTraversalInReverse = new LevelOrderTraversalInReverse(writerMock);
+            levelOrderTraversalInReverse.Print(rootNode);
+        }
+
+        [Test]
+        public void ShouldPrintTreeInLevelOrderForComplexTree()
+        {
+            var expectedOutputs = new List<int>() { 8, 7, 6, 5, 4, 3, 2, 1 };
+            IConsoleWriter writerMock = MockWriter.GetInstance(expectedOutputs);
+            var rootNode = TreeTestData.GetComplexTreeRootNode();
+            var levelOrderTraversalInReverse = new LevelOrderTraversalInReverse(writerMock);
+            levelOrderTraversalInReverse.Print(rootNode);
+        }
+
+    }
+}
