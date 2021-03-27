cf0b729d7259a2527397df97bda119b0e99a46e8
vishnu <vishnusankar.sakthivel@gmail.com>
2021-03-27 11:44:24 +0530

    Added diameter of tree

---
+++ Tree/Problems/DiameterOfTree.cs
@@ -0,0 +1,32 @@
+﻿using Datastructure.Tree.Model;
+using Datastructure.Tree.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.Tree.Test
+{
+    public class DiameterOfTree
+    {
+        public DiameterOfTree()
+        {
+        }
+
+        public int Find(TreeDSNode root)
+        {
+            if (root == null)
+                return 0;
+
+            HeightOfTree heightOfTree = new HeightOfTree();
+            var leftHeight = heightOfTree.UsingRecursion(root.Left);
+            var rightHeight = heightOfTree.UsingRecursion(root.Right);
+
+            var leftDiameter = Find(root.Left);
+            var rightDiameter = Find(root.Right);
+
+            return Math.Max(1 + leftHeight + rightHeight, Math.Max(leftDiameter, rightDiameter));
+        }
+    }
+}
---
+++ Tree/Test/DiameterOfTreeTest.cs
@@ -0,0 +1,28 @@
+﻿using Datastructure.Tree.Model;
+using Datastructure.Tree.Problems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.Tree.Test
+{
+    [TestFixture]
+    public class DiameterOfTreeTest
+    {
+        public DiameterOfTreeTest()
+        {
+        }
+
+        [Test]
+        public void ShouldReturnDiameterOfTree()
+        {
+            TreeDSNode rootNode = TreeTestData.GetComplexTreeRootNode();
+            DiameterOfTree diameterOfTree = new DiameterOfTree();
+            var diameter = diameterOfTree.Find(rootNode);
+            Assert.AreEqual(6, diameter);
+        }
+    }
+}
