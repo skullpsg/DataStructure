cdb5e517d3fa73a2391b5a07b5a3b79146eadc76
vishnu <vishnusankar.sakthivel@gmail.com>
2021-03-29 13:53:50 +0530

    Added summing all nodes

---
+++ Tree/Problems/SumAllNodesInTree/SumAllNodesInTest.cs
@@ -0,0 +1,29 @@
+﻿using Datastructure.Tree.Problems.Test.Data;
+using NUnit.Framework;
+
+
+namespace Datastructure.Tree.Problems.SumAllNodesInTree
+{
+    [TestFixture]
+    public class SumAllNodesInTest
+    {
+        [Test]
+        public void ShouldReturnSumUsingRecussion()
+        {
+            var rootNode = TreeTestData.GetComplexTreeRootNode();
+            var sumAllNode = new SumAllNodesInTree();
+            var sum = sumAllNode.UsingRecursion(rootNode);
+            Assert.AreEqual(36, sum);
+        }
+
+        [Test]
+        public void ShouldReturnSumUsingNonRecussion()
+        {
+            var rootNode = TreeTestData.GetComplexTreeRootNode();
+            var sumAllNode = new SumAllNodesInTree();
+            var sum = sumAllNode.UsingNonRecursion(rootNode);
+            Assert.AreEqual(36, sum);
+        }
+
+    }
+}
---
+++ Tree/Problems/SumAllNodesInTree/SumAllNodesInTree.cs
@@ -0,0 +1,51 @@
+﻿using Datastructure.Common;
+using Datastructure.Tree.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.Tree.Problems.SumAllNodesInTree
+{
+    public class SumAllNodesInTree : IAlgorithm<TreeDSNode, int>
+    {
+        public int UsingNonRecursion(TreeDSNode root)
+        {
+            int sum = 0;
+            TreeDSNode currentNode = null;
+            var queue = new Queue<TreeDSNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                currentNode = queue.Dequeue();
+
+                if (currentNode != null)
+                    sum += currentNode.Value;
+
+                if (currentNode.isLeftNodeAvailable())
+                {
+                    queue.Enqueue(currentNode.Left);
+                }
+
+                if (currentNode.isRightNodeAvailable())
+                {
+                    queue.Enqueue(currentNode.Right);
+                }
+            }
+            return sum;
+        }
+
+        public int UsingRecursion(TreeDSNode root)
+        {
+            if (root == null)
+                return 0;
+
+            if (root.isLeftNodeNull() && root.isRightNodeAvailable())
+                return root.Value;
+
+            return root.Value + UsingRecursion(root.Left) + UsingRecursion(root.Right);
+
+        }
+    }
+}
