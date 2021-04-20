b28e2e53cfbe3771a88e16c55a61343034a1df20
vishnu <vishnusankar.sakthivel@gmail.com>
2021-04-20 13:37:24 +0530

    added dutch man flag

---
+++ Array/Problem/Sort3Digits/Sort3Digits.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.Array.Problem.Sort3Digits
+{
+    public class Sort3Digits
+    {
+        private IConsoleWriter console;
+        public Sort3Digits(IConsoleWriter console = null)
+        {
+            this.console = console;
+        }
+
+        public void Sort(int[] array)
+        {
+            if (array.Length == 0)
+                return;
+
+            void swap(int from, int to)
+            {
+                var temp = array[from];
+                array[from] = array[to];
+                array[to] = temp;
+            }
+
+            int low = 0;
+            int mid = 0;
+            int high = array.Length - 1;
+
+            while (mid < high)
+            {
+                if (array[mid] > array[high])
+                {
+                    swap(mid++, high--);
+                }
+                else if (array[low] > array[mid])
+                {
+                    swap(low++, mid++);
+                }
+                else if (array[mid] == array[high])
+                {
+                    high--;
+                }
+                else
+                {
+                    mid++;
+                }
+            }
+            Print(array);
+        }
+
+        private void Print(int[] array)
+        {
+            int i = 0;
+            int length = array.Length - 1;
+            while (i < length)
+            {
+                console.WriteLine(array[i]);
+                i++;
+            }
+        }
+
+    }
+}
---
+++ Array/Problem/Sort3Digits/Sort3DigitsTest.cs
@@ -0,0 +1,34 @@
+﻿using Datastructure.Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.Array.Problem.Sort3Digits
+{
+    class Sort3DigitsTest
+    {
+
+        [Test]
+        public void ShouldSort3DigitUsingDutchFlagForSortedNumbers()
+        {
+            var expectedOutputs = new List<int>() { 1, 2, 3 };
+            IConsoleWriter writerMock = MockWriter.GetInstance(expectedOutputs);
+            var Sort3Digits = new Sort3Digits(writerMock);
+            var input = new int[] { 1, 2, 3 };
+            Sort3Digits.Sort(input);
+        }
+
+        [Test]
+        public void ShouldSort3DigitUsingDutchFlag()
+        {
+            var expectedOutputs = new List<int>() { 0, 0, 1, 1, 1, 2, 2 };
+            IConsoleWriter writerMock = MockWriter.GetInstance(expectedOutputs);
+            var Sort3Digits = new Sort3Digits(writerMock);
+            var input = new int[] { 1, 0, 2, 0, 1, 1, 2 };
+            Sort3Digits.Sort(input);
+        }
+    }
+}
