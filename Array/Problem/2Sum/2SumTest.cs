4429130885f82876ed3b32970e9451fd393871f6
vishnu <vishnusankar.sakthivel@gmail.com>
2021-05-07 14:50:53 +0530

    added two sum brute force method

---
+++ Array/Problem/2Sum/2SumTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.Array.Problem._2Sum
+{
+    [TestFixture]
+    class TwoSumTest
+    {
+        [Test]
+        public void ShouldReturnIndexes()
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            TwoSum twoSum = new TwoSum();
+            int target = 9;
+            var expectedResult = new int[] { 0, 1 };
+            var actualResult = twoSum.GetSum(nums, target);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ Array/Problem/2Sum/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.Array.Problem
+{
+    internal class TwoSum
+    {
+        public int[] GetSum(int[] nums, int target)
+        {
+            for (int index = 0; index < nums.Length; index++)
+            {
+                for (int secondIndex = index + 1; index < nums.Length; index++)
+                {
+                    if (nums[index] + nums[secondIndex] == target)
+                    {
+                        return new int[] { index, secondIndex };
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
