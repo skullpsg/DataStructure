be68ad54beff41592c3810966dd5a92dbcc5738a
vishnu <vishnusankar.sakthivel@gmail.com>
2021-08-30 12:19:48 +0530

    added change coins possibilities

---
+++ DynamicProblem/CoinChange/CoinChangeTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Datastructure.DynamicProblem.CoinChange
+{
+    [TestFixture]
+    class CoinChangeTest
+    {
+
+        [Test]
+        public void ShouldReturnPaths() {
+            CoinsChange coins = new CoinsChange();
+            int[] coinAvaliable = new int[] { 1, 2, 5 };
+            int possibilities = coins.GetChangePossibilities(5, 0, coinAvaliable);
+            Assert.AreEqual(possibilities, 4);
+        }
+    }
+}
---
+++ DynamicProblem/CoinChange/CoinsChange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.DynamicProblem
+{
+    public class CoinsChange
+    {
+   
+        int[,] cache = new int[6,6];
+        public int GetChangePossibilities(int amount,int index, int[] coins){
+            if (cache[amount, index] > 0) {
+                return cache[amount, index];
+            }
+            int paths = 0;
+            if (amount == 0)
+            {
+                return 1;
+            }
+
+            if (index >= coins.Length)
+                return 0;
+            for (int loopCount = 0; loopCount * coins[index] <= amount; loopCount++) {
+                paths += GetChangePossibilities(amount - (loopCount * coins[index]), index + 1, coins);
+             }
+            cache[amount, index]= paths;
+            return paths;
+        }
+    }
+}
