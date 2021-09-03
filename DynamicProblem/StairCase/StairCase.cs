3ec06cd4c1a3a9ace8c648bb9cc6e66726e1acb2
vishnu <vishnusankar.sakthivel@gmail.com>
2021-09-03 08:41:56 +0530

    Stair case problem added

---
+++ DynamicProblem/StairCase/StairCase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Datastructure.DynamicProblem.StairCase
+{
+    public class StairCase
+    {
+        public int GetPossibilities(int NoOfSteps, Dictionary<int, int> cache)
+        {
+
+            if (NoOfSteps < 0)
+            {
+                return 0;
+            }
+            if (cache.ContainsKey(NoOfSteps))
+            {
+                return cache[NoOfSteps];
+            }
+
+            if (NoOfSteps == 0)
+            {
+                return 1;
+            }
+            int result = GetPossibilities(NoOfSteps - 1,cache) + GetPossibilities(NoOfSteps - 2,cache) + GetPossibilities(NoOfSteps - 3,cache);
+            cache.Add(NoOfSteps, result);
+            return result;
+        }
+    }
+}
---
+++ DynamicProblem/StairCase/StairCaseTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.DynamicProblem.StairCase
+{
+    [TestFixture]
+    class StairCaseTest
+    {
+        [Test]
+        public void ShouldReturnNoOfPossibilities() {
+            var stairCase = new StairCase();
+            int possibilities = stairCase.GetPossibilities(3,new Dictionary<int, int>());
+            Assert.AreEqual(4,possibilities);
+        }
+
+    }
+}
