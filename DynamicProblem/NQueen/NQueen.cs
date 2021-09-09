c3c39dd65e5f8000d7c8690c45b2937f330279c8
vishnu <vishnusankar.sakthivel@gmail.com>
2021-09-09 19:08:53 +0530

    Added nQueenProblem

---
+++ DynamicProblem/NQueen/NQueen.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.DynamicProblem.NQueen
+{
+    public class NQueen
+    {
+        public IConsoleWriter console = null;
+
+        public NQueen(IConsoleWriter console = null)
+        {
+            this.console = console;
+        }
+
+        public bool PlaceNQueen(int[] cols, int row)
+        {
+            if (row == cols.Length)
+            {
+                print(cols);
+                return true;
+            }
+
+            for (int i = 0; i < cols.Length; i++)
+            {
+                if (isSafe(cols, row, i))
+                {
+                    cols[row] = i;
+                    if (PlaceNQueen(cols, row + 1))
+                        return true;
+                    cols[row] = -1;
+                }
+            }
+            return false;
+        }
+
+        private void print(int[] cols)
+        {
+            for (int row = 0; row < cols.Length; row++)
+            {
+                console.WriteLine($"{row},{cols[row]}");
+            }
+        }
+
+        bool isSafe(int[] cols, int row, int col)
+        {
+            for (int row2 = 0; row2 < row; row2++)
+            {
+                int col2 = cols[row2];
+                if (col2 == col)
+                {
+                    return false;
+                }
+                int colDist = Math.Abs(col2 - col);
+                int rowDist = Math.Abs(row2 - row);
+                if (rowDist == colDist)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ DynamicProblem/NQueen/NQueenTest.cs
@@ -0,0 +1,24 @@
+﻿using Datastructure.Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.DynamicProblem.NQueen
+{
+    [TestFixture]
+    public class NQueenTest
+    {
+        [Test]
+        public void ShouldPrintNQueen()
+        {
+            var expectedOutputs = new List<String>() { "0,0", "1,4", "2,7", "3,5", "4,2", "5,6", "6,1", "7,3" };
+            IConsoleWriter writerMock = MockWriter.GetInstanceForString(expectedOutputs);
+            NQueen nqueen = new NQueen(writerMock);
+            int[] cols = new int[8] { -1, -1, -1, -1, -1, -1, -1, -1 };
+            nqueen.PlaceNQueen(cols, 0);
+        }
+    }
+}
