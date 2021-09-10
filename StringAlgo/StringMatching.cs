a1077f17599c8f6c7587b24d063fc1bf11889287
vishnu <vishnusankar.sakthivel@gmail.com>
2021-09-10 08:06:27 +0530

    Added brute force approach for string matching

---
+++ StringAlgo/StringMatching.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.StringAlgo
+{
+    public class StringMatching
+    {
+        public int MatchInBruteForce(string str1, string str2)
+        {
+            if (str1 == null && str2 == null)
+                return 0;
+
+            for (int index = 0; index < str1.Length; index++)
+            {
+                int innerStringIndex = 0;
+                while (innerStringIndex < str2.Length && str1[index + innerStringIndex] == str2[innerStringIndex])
+                {
+                    innerStringIndex++;
+                }
+                if (innerStringIndex == str2.Length)
+                {
+                    return index;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ StringAlgo/StringMatchingTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Datastructure.StringAlgo
+{
+
+    [TestFixture]
+    public class StringMatchingTest
+    {
+        [Test]
+        public void ShouldMatchStringInBruteForce()
+        {
+            string str1 = "vishnusankarsakthivel";
+            string str2 = "sakthivel";
+            var stringMatcher = new StringMatching();
+            int stratingIndex = stringMatcher.MatchInBruteForce(str1, str2);
+
+            Assert.AreEqual(stratingIndex,12);
+        }
+    }
+}
