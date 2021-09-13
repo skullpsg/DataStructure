2430cfefea2c42ff686950ace1fd708ba63797c0
vishnu <vishnusankar.sakthivel@gmail.com>
2021-09-13 18:08:36 +0530

    Added Addition(on particular index) and subtraction

---
+++ Array/Problem/SumOfDifference/SumOfDifference.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.Array.Problem.SumOfDifference
+{
+    public class SumOfDifference
+    {
+
+        public int Sum(int[] arr)
+        {
+            int result = 0;
+            int indexToAdd = 0;
+            int count = 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (indexToAdd == i)
+                {
+                    result += arr[i];
+                    indexToAdd += count;
+                    count++;
+                }
+                else
+                {
+                    result -= arr[i];
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Array/Problem/SumOfDifference/SumOfDifferenceTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.Array.Problem.SumOfDifference
+{
+    [TestFixture]
+    public class SumOfDifferenceTest
+    {
+        [Test]
+        public void ShouldReturnSumOfDiff()
+        {
+            int[] arr = new int[2] { 2, 1 };
+            var sum = new SumOfDifference();
+            int resulted = sum.Sum(arr);
+            Assert.AreEqual(resulted, 3);
+        }
+    }
+}
